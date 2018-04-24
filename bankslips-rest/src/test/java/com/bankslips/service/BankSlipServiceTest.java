59a2f444f5dae724b1c9a6e096f6fb8d5d3a3075
igorh <igorh@192.168.0.110>
2018-04-23 22:10:21 -0300

    update junit

--- bankslips-rest/src/test/java/com/bankslips/service/BankSlipServiceTest.java
+++ bankslips-rest/src/test/java/com/bankslips/service/BankSlipServiceTest.java
@@ -4,11 +4,14 @@ import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -22,6 +25,7 @@ import com.bankslips.model.Status;
 import com.bankslips.repository.BankSlipRepository;
 import com.bankslips.service.impl.BankSlipServiceImpl;
 import com.bankslips.support.exception.BankslipException;
+import com.bankslips.support.handler.Messages;
 import com.bankslips.support.validation.BeanValidator;
 
 @RunWith(SpringRunner.class)
@@ -33,6 +37,9 @@ public class BankSlipServiceTest {
 
 	@Value("${bankslip.invalid.fields.code}")
 	private Integer invalidFieldsCode;
+
+	@Value("${bankslip.not.found.code}")
+	private Integer bankSlipNotFound;
 
 	@Autowired
 	private BankSlipService bankSlipService;
@@ -62,19 +69,46 @@ public class BankSlipServiceTest {
 	@Test
 	public void insertBankSlipExceptionTest() {
 		thrown.expect(BankslipException.class);
-		thrown.expect(hasProperty("code"));
 		thrown.expect(hasProperty("code", is(bankSlipNotProvider)));
 		bankSlipService.insert(new BankSlip());
 	}
 
 	@Test
 	public void insertBankSlipValidationError() {
-		// thrown.expect(BankslipException.class);
-		// thrown.expect(hasProperty("code"));
-		// thrown.expect(hasProperty("code", is(invalidFieldsCode)));
-		// BankSlip bankslip = newBankSlip();
-		// bankslip.setCustomer(null);
-		// bankSlipService.insert(bankslip);
+
+		thrown.expect(BankslipException.class);
+		thrown.expect(hasProperty("code", is(invalidFieldsCode)));
+		BankSlip bankslip = newBankSlip();
+		bankslip.setCustomer(null);
+		Mockito.when(validator.validate(bankslip))
+				.thenThrow(new BankslipException(invalidFieldsCode, Messages.FIELD_INVALID.getCode()));
+		bankSlipService.insert(bankslip);
+	}
+
+	@Test
+	public void payBankSlipSucessTest() {
+		Mockito.when(bankSlipRepository.findById(ArgumentMatchers.any())).thenReturn(Optional.of(newBankSlip()));
+		bankSlipService.payBankSlip(newBankSlip().getId());
+	}
+
+	@Test
+	public void payBankSlipExceptionTest() {
+		thrown.expect(BankslipException.class);
+		thrown.expect(hasProperty("code", is(bankSlipNotFound)));
+		bankSlipService.payBankSlip(newBankSlip().getId());
+	}
+
+	@Test
+	public void cancelBankSlipSucessTest() {
+		Mockito.when(bankSlipRepository.findById(ArgumentMatchers.any())).thenReturn(Optional.of(newBankSlip()));
+		bankSlipService.cancelBankSlip(newBankSlip().getId());
+	}
+
+	@Test
+	public void cancelBankSlipExceptionTest() {
+		thrown.expect(BankslipException.class);
+		thrown.expect(hasProperty("code", is(bankSlipNotFound)));
+		bankSlipService.cancelBankSlip(newBankSlip().getId());
 	}
 
 	private BankSlip newBankSlip() {
